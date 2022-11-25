214db0379acd432370d2ff3d6924ba5b9ad627ea
Sergsok1984 <106656255+Sergsok1984@users.noreply.github.com>
2022-11-25 16:28:26 +0300

    Update Seminar9/Task2/logger.py
    Co-authored-by: Danil <49741194+samodurOFF@users.noreply.github.com>

--- Seminar9/Task2/logger.py
+++ Seminar9/Task2/logger.py
@@ -5,8 +5,8 @@ from datetime import datetime
 
 def log(update: Update, context: ContextTypes):
     file = open('log.csv', 'a', encoding='utf-8')
-    file.write(f'{update.effective_user.first_name}', 
-               f'{update.effective_user.id}', 
-               f'{update.message.text}', 
+    file.write(f'{update.effective_user.first_name}' 
+               f'{update.effective_user.id}'
+               f'{update.message.text}'
                f'{datetime.now().strftime("%Y.%m.%d %H:%M:%S")}\n')
     file.close()
